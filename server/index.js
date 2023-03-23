b2ca0f1a04f65ff27a4b2ba0777c595db1e90042
Jiovany Vincent <jiovanyvincent.dev@outlook.com>
2023-03-23 10:59:01 +0100

    🎉 Init project client + server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,26 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const bodyParser = require('body-parser');
 const routes = require('./src/routes');
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
 
 app.use('/', routes);
 
-server.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on('chat-message', msg => {
+    console.log(msg);
+  })
+//  socket.on('chat message', (author, msg) => {
+//   io.emit('chat message', author, msg);
+//  });
+});
+
+io.listen(3000, () => {
   console.log('listening on *:3000');
 });
